51672e32eaf9606cfa182c5c39156a537dd48363
PrajakSritong <prajak.sr@kkumail.com>
2025-07-31 00:07:35 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,210 @@
-import Image from "next/image";
-
+import Marquee from "react-fast-marquee";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <div
+      style={{
+        backgroundImage: "url('/pink-moon.jpg')",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+      }}
+      className="relative w-full min-h-screen overflow-x-hidden"
+    >
+      {/* Navbar */}
+      <nav className="bg-gray-700 text-white py-3 px-4 flex items-center justify-between fixed top-0 left-0 w-full z-10">
+        <a className="font-bold text-xl tracking-tight md:px-40 text-pink-800">
+          PJ.
+        </a>
+        <div className="flex items-center md:px-40">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700"
+            href="#"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Home
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700"
+            href="#about"
           >
-            Read our docs
+            About
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </nav>
+      {/* Hero Section */}
+      <div className="flex flex-col md:flex-row items-center justify-between md:px-40 px-4 pt-32 gap-8 mb-20">
+        <div className="flex-1">
+          <p className="text-2xl text-white mb-2">Hello, I'm</p>
+          <h1 className="text-4xl md:text-6xl bg-gradient-to-r from-red-500 via-violet-500 to-pink-500 bg-clip-text text-transparent font-bold">
+            Prajak Sritong
+          </h1>
+          <h2 className="text-xl md:text-2xl text-gray-400 mt-3 mb-2">
+            Web Developer, Software Engineering <br /> Student
+          </h2>
+          <h2 className="text-gray-400 mt-3 mb-2">
+            Solving intricate problems with innovative and <br /> streamlined
+            tech solutions.
+          </h2>
+        </div>
+
+        {/* Terminal Card */}
+        <div className="card flex-1 max-w-md w-full">
+          <div className="wrap">
+            <div className="terminal">
+              <hgroup className="head flex items-center justify-between">
+                <p className="title flex items-center gap-2">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16px"
+                    width="16px"
+                  >
+                    <path d="M7 15L10 12L7 9M13 15H17M7.8 21H16.2C17.8802 21 18.7202 21 19.362 20.673C19.9265 20.3854 20.3854 19.9265 20.673 19.362C21 18.7202 21 17.8802 21 16.2V7.8C21 6.11984 21 5.27976 20.673 4.63803C20.3854 4.07354 19.9265 3.6146 19.362 3.32698C18.7202 3 17.8802 3 16.2 3H7.8C6.11984 3 5.27976 3 4.63803 3.32698C4.07354 3.6146 3.6146 4.07354 3.32698 4.63803C3 5.27976 3 6.11984 3 7.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21Z" />
+                  </svg>
+                  Terminal
+                </p>
+                <button type="button" tabIndex={-1} className="copy_toggle">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16px"
+                    width="16px"
+                  >
+                    <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+                    <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+                  </svg>
+                </button>
+              </hgroup>
+              <div className="body">
+                <pre className="pre">
+                  <code>-&nbsp;</code>
+                  <code>npm&nbsp;</code>
+                  <code className="cmd"></code>
+                </pre>
+                <pre className="pre">
+                  <code>-&nbsp;</code>
+                  <code>npx&nbsp;</code>
+                  <code className="cmd"></code>
+                </pre>
+                <pre className="pre">
+                  <code>-&nbsp;</code>
+                  <code>coust&nbsp;</code>
+                  <code className="cmd"></code>
+                </pre>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Marquee Section */}
+      <div className=" py-10 mx-30">
+        <Marquee speed={200}>
+          <div className="gap-40 flex items-center justify-center mx-10">
+            <img className="w-24 h-24 rounded-sm" src="/Python.png" />
+            <img className="w-24 h-24 rounded-sm" src="/HTML5.png" />
+            <img className="w-24 h-24 rounded-sm" src="/Next.js.png" />
+            <img className="w-24 h-24 rounded-sm" src="/PHP.png" />
+            <img className="w-24 h-24 rounded-sm" src="/Tailwind CSS.png" />
+            <img className="w-24 h-24 rounded-sm" src="/JavaScript.png" />
+          </div>
+        </Marquee>
+      </div>
+      {/* About Section */}
+      <div
+        id="about"
+        className=" container mx-auto mt-35 p-4 rounded-lg shadow-lg pb-14"
+      >
+        <h1 className="text-6xl font-bold  text-white mb-4">About Me</h1>
+        <h3 className="text-xl font-semibold mb-2 flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-file-text w-5 h-5 text-primary"
+          >
+            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+            <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+            <path d="M10 9H8"></path>
+            <path d="M16 13H8"></path>
+            <path d="M16 17H8"></path>
+          </svg>{" "}
+          Bio
+        </h3>
+        <div className=" mb-10">
+          <p className="text-gray-300">
+            I'm a passionate web developer with a love for creating beautiful
+            and functional user experiences. My journey in tech started with a
+            curiosity for how things work, and it has evolved into a
+            full-fledged career where I get to solve problems and bring ideas to
+            life.
+          </p>
+        </div>
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col gap-1 ring-2 ring-pink-500/50 shadow-xl px-4 inset-ring-2 ring-pink-500 shadow-pink-500/50 py-4">
+            <h3 className="text-lg font-semibold mb-3 flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-graduation-cap w-5 h-5 text-primary"
+              >
+                <path d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"></path>
+                <path d="M22 10v6"></path>
+                <path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5"></path>
+              </svg>{" "}
+              Education
+            </h3>
+            <p>Khon Kaen University</p>
+            <p>Faculty of Interdisciplinary Studies</p>
+            <p>Department of Computer Science and Information</p>
+            <p>2022 ~ present</p>
+          </div>
+          <div className="flex flex-col gap-1 ring-2 ring-pink-500/50 shadow-xl px-4 inset-ring-2 ring-pink-500 shadow-pink-500/50 py-4">
+            <h3 className="text-lg font-semibold mb-3 flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-briefcase w-5 h-5 text-primary"
+              >
+                <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+                <rect width="20" height="14" x="2" y="6" rx="2"></rect>
+              </svg>{" "}
+              Coursework
+            </h3>
+            <p>Designed databases and wrote SQL queries</p>
+            <p>Developed websites using HTML, CSS, and JavaScript</p>
+            <p>Implemented server-side programming with database integration</p>
+            <p>Developed mobile applications</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
